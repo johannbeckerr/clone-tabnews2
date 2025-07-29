859cefee3d432eac0c2a54c2f8c72a3f0533f4ae
Johann Becker <johann.drums@gmail.com>
2025-07-29 08:40:01 +0000

    update index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     return <div>
-                <h1>Te amo, minha italiana!</h1>
+                <h1>Te amo, minha italiana 🇧🇷🇮🇹</h1>
                 <img src="https://media.tenor.com/U9r4VK8_gOkAAAAj/sunflower-pvz.gif" 
                 alt="Girassol" 
                 class="sunflower-image"></img>
